d3eddf39a59cbdf51ff3e3e3ad7a12c7d8f27878
ivinnichek <vinnichekira@gmail.com>
2017-12-03 15:07:05 +0300

    Add lab4

---
+++ lab4/Selenium.Logic/MainPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System.Linq;
+using System.Threading;
+
+namespace Selenium.Pages
+{
+    public class MainPage : Page
+    {
+        public readonly string MAIN_URL = "https://tumblr.com/";
+
+        public MainPage(IWebDriver driver) : base(driver) { }
+
+        public MainPage Open()
+        {
+            driver.Navigate().GoToUrl(MAIN_URL);
+            return this;
+        }
+
+        public FinderPage FindPostsByTag()
+        {
+            IWebElement tag = driver.FindElement(By.ClassName("search_query"));
+            tag.Click();
+            tag.SendKeys("girl");
+            tag.SendKeys(Keys.Enter);
+            return new FinderPage(driver);
+        }
+    }
+}
---
+++ lab4/Selenium.Logic/Page.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace Selenium.Pages
+{
+    public abstract class Page
+    {
+        protected IWebDriver driver;
+
+        public Page(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+    }
+}
