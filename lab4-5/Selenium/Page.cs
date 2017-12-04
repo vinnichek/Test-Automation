8d2490886715b47c8e8b4a9c1858fe9160a79f2d
ivinnichek <vinnichekira@gmail.com>
2017-12-04 23:52:20 +0300

    Rafactor

---
+++ lab4-5/Selenium/Page.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace Selenium
+{
+    public abstract class Page
+    {
+        protected IWebDriver driver;
+
+        public Page(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+    }
+}
